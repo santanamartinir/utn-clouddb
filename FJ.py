6758b17d40042c2173edf0b7cdfc73d579ee0649
Irene Santana Martin <irenesantanamartin01@gmail.com>
2024-07-23 15:47:55 +0200

    update on FJ.py

--- FJ.py
+++ FJ.py
@@ -23,12 +23,14 @@ def partition(data, num_partitions):
 
 # Integration with high-speed networks (RDMA)
 
-if __name__ == "__main__":
-    num_tuples = 10
-    skew_factor = 0.5
-    data = generate_data(num_tuples, skew_factor)
+# FlowJoin
+
+
+num_tuples = 10
+skew_factor = 0.5
+data = generate_data(num_tuples, skew_factor)
     
-    # Print
-    print("Generated data:")
-    for item in data:
-        print(item)
+# Print
+print("Generated data:")
+for item in data:
+    print(item)

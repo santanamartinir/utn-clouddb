d84d84450cd5444836f9dabe5359f61bc801e997
Irene Santana Martin <irenesantanamartin01@gmail.com>
2024-07-29 01:09:49 +0200

    remove partition.py

--- partition.py
+++
@@ -1,9 +0,0 @@
-from collections import defaultdict
-
-def partition_data(data, num_partitions):
-    partitions = defaultdict(list)  # Diccionario para almacenar particiones
-    for key, value in data:
-        partition = key % num_partitions  # Asignamos partición basada en la clave
-        partitions[partition].append((key, value))  # Añadimos la tupla a la partición correspondiente
-    print(f"Particiones después de particionar {data}: {dict(partitions)}")
-    return partitions  # Devolvemos las particiones
